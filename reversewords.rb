05852535bf2c7a22d5107f3a8bbe712d2b686bb9
“Tyler <tylerpalef@gmail.com>
2018-10-16 11:19:39 -0400

    found a solution on stackoverflow that introduces .scan and .map, other methods to call on strings I did not use

--- reversewords.rb
+++ reversewords.rb
@@ -1,13 +1,23 @@
+# def reverse_words(str)
+#   new_string = []
+#   str.split.each do |x|
+#     if x == new_string.last
+#       new_string << x.reverse
+#       else new_string << x.reverse + ' '
+#         end
+#       end
+#   new_string.join.chomp
+# end
+#
+#
+# puts reverse_words('hello my dolly')
+
+# Solution:
+# https://stackoverflow.com/questions/52578068/reversing-characters-in-a-string-regardless-of-the-number-of-spaces-ruby
+
 def reverse_words(str)
-  new_string = []
-  str.split.each do |x|
-    if x == new_string.last
-      new_string << x.reverse
-      else new_string << x.reverse + ' '
-        end
-      end
-  new_string.join.chomp
+  str
+    .scan(/(\s*)(\S+)(\s*)/)
+    .map { |spacer1, word, spacer2| spacer1 + word.reverse + spacer2 }
+    .join
 end
-
-
-puts reverse_words('hello my dolly')
